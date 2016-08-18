4682644a0b11283a9978efacea03e941f2c4a426
yezhoucc <1066373658@qq.com>
2016-08-18 23:40:59 +0800

    slider

---
+++ slider/01.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+	$pics = array(
+		'./upload/new1.jpg',
+		'./upload/new2.jpg',
+		'./upload/new3.jpg',
+		'./upload/new4.jpg',
+		'./upload/new5.jpg'
+	);
+	echo json_encode($pics);
+
+
+
+
+
+?>
